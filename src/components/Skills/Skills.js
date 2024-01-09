adb586e2c69aad504e0ab50048fe3b7aa945d6ca
Madhukar kumar <madhukarkumar2468@gmail.com>
2024-01-09 12:29:34 +0530

    code

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -54,7 +54,7 @@ const otherSkills = [
     {
         name: "Git",
         image:
-            "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+            "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
     },
     {
         name: "Bootstrap",
@@ -79,6 +79,11 @@ const otherSkills = [
         name: "Netlify",
         image:
             "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+    },
+    {
+        name: "Vercel",
+        image:
+            "https://karmanivero.us/assets/images/logo-vercel.png",
     },
     {
         name: "VS Code",
